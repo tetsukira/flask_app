45893c2729c14c892aa1345ff11e168f3df78bf3
吉良哲 <tetsu@kira.AirPort>
2023-03-04 16:13:19 +0900

    first commit

--- src/view.py
+++ src/view.py
@@ -11,8 +11,10 @@ app = Flask(__name__)
 @app.route('/')
 # def以下がアクセス後の操作
 def index():
+    title_ = 'ようこそ'
+    message_ = 'MTVデザインパターンでWebアプリ作成'
     # return 'Hello World!'
-    return render_template('index.html') #追加
+    return render_template('index.html', title = title_, message= message_) #追加
 
 
 if __name__ == '__main__':
